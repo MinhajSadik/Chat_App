f60cb68e95f60408c5a0cec210119c4c64532530
MinhajSadik <MinhajoSadik@gmail.com>
2022-05-08 19:12:53 +0600

    [ADD] worked done login for this time

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,55 +1,83 @@
-const User = require("../models/People");
-const bcrypt = require('bcrypt');
+// external imports
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const createError = require("http-errors");
-const res = require("express/lib/response");
 
-//get login page
+// internal imports
+const User = require("../models/People");
+
+// get login page
 function getLogin(req, res, next) {
   res.render("index");
 }
 
-//do login
+// do login
 async function login(req, res, next) {
   try {
+    // find a user who has this email/username
     const user = await User.findOne({
       $or: [{ email: req.body.username }, { mobile: req.body.username }],
-      if(user && user._id){
-        const isPasswordValid = await bcrypt.compare(req.body.password, user.password);
-        if(isPasswordValid){
-          const userObject = {
-            username: user.name,
-            mobile: user.mobile,
-            email: user.email,
-            role: "user",
-          }
-          const token = jwt.sign(userObject, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRY });
-          res.cookie(process.env.COOKIE_NAME, token, { httpOnly: true, maxAge: process.env.JWT_EXPIRY, signed: true });
-          res.locals.loggedInUser = userObject;
-          res.render('inbox');
-        }else{
-          throw createError("login failed! please try again.")
-        }
-      }else{
-        throw createError("login failed! please try again.")
-      }
     });
+
+    if (user && user._id) {
+      const isValidPassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+
+      if (isValidPassword) {
+        // prepare the user object to generate token
+        const userObject = {
+          username: user.name,
+          mobile: user.mobile,
+          email: user.email,
+          role: "user",
+        };
+
+        // generate token
+        const token = jwt.sign(userObject, process.env.JWT_SECRET, {
+          expiresIn: process.env.JWT_EXPIRY,
+        });
+
+        // set cookie
+        res.cookie(process.env.COOKIE_NAME, token, {
+          maxAge: process.env.JWT_EXPIRY,
+          httpOnly: true,
+          signed: true,
+        });
+
+        // set logged in user local identifier
+        res.locals.loggedInUser = userObject;
+
+        res.render("inbox");
+      } else {
+        throw createError("Login failed! Please try again.");
+      }
+    } else {
+      throw createError("Login failed! Please try again.");
+    }
   } catch (err) {
     res.render("index", {
-      data:{
+      data: {
         username: req.body.username,
-
-      }
+      },
       errors: {
         common: {
           msg: err.message,
-        }
-      }
+        },
+      },
     });
   }
 }
 
+// do logout
+function logout(req, res) {
+  res.clearCookie(process.env.COOKIE_NAME);
+  res.send("logged out");
+}
+
 module.exports = {
   getLogin,
-  login
+  login,
+  logout,
 };
