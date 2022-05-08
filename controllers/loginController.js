0693a08ad8f79a7b0e9909c4099fa9333852d28e
MinhajSadik <MinhajoSadik@gmail.com>
2022-05-08 17:35:58 +0600

    [ADD] working for login handling router and controller and middleware related worked done

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,8 +1,55 @@
+const User = require("../models/People");
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+const createError = require("http-errors");
+const res = require("express/lib/response");
+
 //get login page
 function getLogin(req, res, next) {
   res.render("index");
 }
 
+//do login
+async function login(req, res, next) {
+  try {
+    const user = await User.findOne({
+      $or: [{ email: req.body.username }, { mobile: req.body.username }],
+      if(user && user._id){
+        const isPasswordValid = await bcrypt.compare(req.body.password, user.password);
+        if(isPasswordValid){
+          const userObject = {
+            username: user.name,
+            mobile: user.mobile,
+            email: user.email,
+            role: "user",
+          }
+          const token = jwt.sign(userObject, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRY });
+          res.cookie(process.env.COOKIE_NAME, token, { httpOnly: true, maxAge: process.env.JWT_EXPIRY, signed: true });
+          res.locals.loggedInUser = userObject;
+          res.render('inbox');
+        }else{
+          throw createError("login failed! please try again.")
+        }
+      }else{
+        throw createError("login failed! please try again.")
+      }
+    });
+  } catch (err) {
+    res.render("index", {
+      data:{
+        username: req.body.username,
+
+      }
+      errors: {
+        common: {
+          msg: err.message,
+        }
+      }
+    });
+  }
+}
+
 module.exports = {
   getLogin,
+  login
 };
--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -3,10 +3,25 @@ const express = require("express");
 const router = express.Router();
 
 //internal imports
-const { getLogin } = require("../controllers/loginController");
+const { getLogin, login } = require("../controllers/loginController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const {
+  doLoginValidationHandler,
+  doLoginValidators,
+} = require("../middlewares/login/loginValidator");
+
+const page_title = "Login";
 
 //login page
-router.get("/", decorateHtmlResponse("Login"), getLogin);
+router.get("/", decorateHtmlResponse(page_title), getLogin);
+
+//do login
+router.post(
+  "/",
+  decorateHtmlResponse(page_title),
+  doLoginValidators,
+  doLoginValidationHandler,
+  login
+);
 
 module.exports = router;
